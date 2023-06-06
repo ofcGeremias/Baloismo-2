c7cbd2ff9ade6897755f5cecbab32902d487e72c
geremias <geremiasdorego10@gmail.com>
2023-06-06 12:14:44 -0300

    baloismo

---
+++ Balao.java
@@ -0,0 +1,14 @@
+public class Balao {
+    int id;
+
+    String restritro;
+    String cor;
+    String tipo;
+    double altura;
+    double peso;
+    String porte;
+    int capacidadePessoa;
+    public String voar(Combustivel gasPropano){
+        return "voando com" + gasPropano;
+    }
+}
